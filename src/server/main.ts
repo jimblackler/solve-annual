d5bac144adc59f8e976215dd7bf877c1de6fc739
Jim Blackler <jimblackler@google.com>
2024-12-10 00:06:15 +0000

    Disallow nonexistent dates.

--- src/server/main.ts
+++ src/server/main.ts
@@ -23,12 +23,15 @@ app.route('/day/:month/:day').get(asyncHandler(async (req, res, next) => {
   const month = Number.parseInt(req.params.month);
   const day = Number.parseInt(req.params.day);
   if (month >= 1 && month <= 12 && day >= 1 && day <= 31) {
-    await renderPage(res, async (document, body) => {
-      await renderDay(document, body, month, day);
-    });
-  } else {
-    next();
+    const date = new Date(2024, month - 1, day, 0, 0, 0, 0);
+    if (date.getMonth() === month - 1 && date.getDate() === day) {
+      await renderPage(res, async (document, body) => {
+        await renderDay(document, body, month, day);
+      });
+      return;
+    }
   }
+  next();
 }));
 app.route('/dist/{/*path}').get((req, res) => {
   res.set('Cache-control', `public, max-age=${365 * 24 * 60 * 60}`);
